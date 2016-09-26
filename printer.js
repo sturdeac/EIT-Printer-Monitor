6c21d97c87620b0d90fc62ac7424d0f865093f90
Aaron Mercier <mercieal@rose-hulman.edu>
2016-09-26 15:20:52 -0400

    Added partial paper size
    Added partial paper size

--- printer.js
+++ printer.js
@@ -57,16 +57,16 @@ $.each(printerList, function(index, printer) {
 			if (printer.type === "M806"){
 				// I like these printers, everything has an id...
 				setStatus(printer.hostName, $(result).find('#MachineStatus').text());
-				setTrayLevel(printer.hostName, $(result).find('#TrayBinStatus_2').text(), 2);
-				setTrayLevel(printer.hostName, $(result).find('#TrayBinStatus_3').text(), 3)
+				setTrayLevel(printer.hostName, $(result).find('#TrayBinStatus_2').text(), $(result).find('#TrayBinSize_2').text(), 2);
+				setTrayLevel(printer.hostName, $(result).find('#TrayBinStatus_3').text(), $(result).find('#TrayBinSize_3').text(), 3)
 				setInkLevel(printer.hostName, $(result).find('#SupplyPLR0').text().replace("*", ""));
 				setKitLevel(printer.hostName, $(result).find('#SupplyPLR1').text().replace("*", ""));
 			}
 			else if (printer.type === "9040"){
 				// -__- why no id attributes HP!
 				setStatus(printer.hostName, $(result).find('#deviceStatusPage').children()[0].childNodes[2].textContent);
-				setTrayLevel(printer.hostName, $(result).find('#deviceStatusPage').children()[2].childNodes[2].childNodes[1].childNodes[4].childNodes[3].childNodes[2].textContent, 2);
-				setTrayLevel(printer.hostName, $(result).find('#deviceStatusPage').children()[2].childNodes[2].childNodes[1].childNodes[6].childNodes[3].childNodes[2].textContent, 3);
+				setTrayLevel(printer.hostName, $(result).find('#deviceStatusPage').children()[2].childNodes[2].childNodes[1].childNodes[4].childNodes[3].childNodes[2].textContent, "test", 2);
+				setTrayLevel(printer.hostName, $(result).find('#deviceStatusPage').children()[2].childNodes[2].childNodes[1].childNodes[6].childNodes[3].childNodes[2].textContent,"test", 3);
 				setInkLevel(printer.hostName, $(result).find('.hpGasGaugeBorder')[0].childNodes[1].style.width);
 				setKitLevel(printer.hostName, $(result).find('.hpGasGaugeBorder')[1].childNodes[1].style.width);
 			}
@@ -87,26 +87,39 @@ var setStatus = function(hostName, status){
 			|| status.startsWith("Ready") || status.startsWith("ORDER CARTRIDGE")){
 			status = "Working";
 		}
-		$('#' + hostName + '-status').text("Status - " + status);
+		$('#' + hostName + '-status').text("Status - " + toTitleCase(status));
 };
 
-var setTrayLevel = function(hostName, trayLevel, trayNumber) {
+var setTrayLevel = function(hostName, trayLevel, paperType, trayNumber) {
+	$('#' +hostName + '-tray' + trayNumber + '-type').text(paperType);
 	var imgLevel = -1;
 	if (trayLevel.includes('40 - 100%')){
 		$('#' + hostName + '-tray' + trayNumber).text('40 - 100%');
 		imgLevel = 4;
-		reams += 0;
+		reams += 0.0;
 	}
 	else if (trayLevel.includes('20 - 40%')){
 		$('#' + hostName + '-tray' + trayNumber).text('20 - 40%');
 		imgLevel = 3
-		reams += 0.4
+		reams += 0.6
+	}
+	else if (trayLevel.includes('10 - 20%')){
+		$('#' + hostName + '-tray' + trayNumber).text('10 - 20%');
+		imgLevel = 2
+		reams += 0.8
 	}
 	else if (trayLevel.includes('< 10%') || trayLevel.includes('1 - 10%')){
 		$('#' + hostName + '-tray' + trayNumber).text('1 - 10%');
 		imgLevel = 1
-		reams += 0.1
+		reams += 0.9
 	}
+	else if (trayLevel.includes('Empty')){
+		$('#' + hostName + '-tray' + trayNumber).text('0%');
+		imgLevel = 0
+		reams += 1.0
+	}
+
+	$('#reams').text(" " + reams.toFixed(1));
 
 	if (imgLevel != -1){
 		$('#' + hostName + '-tray' + trayNumber + '-img').attr("src","images/level" + imgLevel + ".gif");
@@ -122,4 +135,8 @@ var setInkLevel = function(hostName, level){
 var setKitLevel = function(hostName, level){
 	$('#' + hostName + '-kitbar').width(level);
 	$('#' + hostName + '-kit').text(level);
+}
+
+function toTitleCase(str){
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
